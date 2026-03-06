6d8076686cc4ca27af9e306c7fb5b8c60290b382
Indra Sawit <noeldycreator@gmail.com>
2026-03-06 07:44:20 +0700

    ci: add type definition and modify gh workflows

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+export function convertMsToDuration(ms: number): string;
+
+export function formatSize(
+  byteCount: number,
+  withPerSecond?: boolean
+): string;
+
+export function generateUID(id: string): string | null;
+
+export function getRandomElement<T>(array: T[]): T | null;
+
+export function isUrl(url: string): boolean;
+
+export function delay(ms: number): Promise<void> | null;
+
+export const formatUptime: (startTime: number) => string;
+
+export const escapeHTML: (text: string) => string;
